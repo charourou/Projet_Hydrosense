f877643b95379bf733defbc1c36ceeda00c2d0c5
Maxime Richard <maxime.richard@gmail.com>
2026-06-01 20:50:58 +0200

    Fix: Mise à jour de entite.py et synchronisation avec master

--- hydrosense/entite.py
+++ hydrosense/entite.py
@@ -7,28 +7,41 @@ class Entite:
         Catalogue pour gérer les métadonnées des stations piézométriques.
         """
         self.url_stations = "https://hubeau.eaufrance.fr/api/v1/niveaux_nappes/stations"
-        self.donnees_brutes = [] # Stockera le JSON brut (liste de dictionnaires)
-        self.catalogue_df = pd.DataFrame() # Stockera le DataFrame structuré
+        self.donnees_brutes = []            # Stockera le JSON brut (liste de dictionnaires)
+        self.catalogue_df = pd.DataFrame()  # Stockera le DataFrame structuré
 
-    def total_stations(self):
+
+    def total_stations(self, date_recherche = None , nb_mesures_piezo_min = 1):
         '''
-        Retourne le compte des stations.
+        Retourne le compte des stations nb_mesures_piezo_min
         '''
-        params = {"format": "json", "size": 1}
+        params = {"format": "json", "size": 1,
+                  "nb_mesures_piezo_min" : nb_mesures_piezo_min
+                  }
+
+        if date_recherche:
+            params['date_recherche'] = date_recherche
 
         reponse = requests.get(self.url_stations, params=params).json()
         total_stations = reponse.get("count")
         print(f"Nombre total de stations trouvées : {total_stations}")
 
-    def rechercher_stations(self, code_dep=None, code_region=None, code_bdlisa=None, taille_max=5000) -> list:
+
+    def rechercher_stations(self, code_dep=None, code_region=None, code_bdlisa=None,
+                            date_recherche= None,
+                            nb_mesures_piezo_min = 1,
+                            taille_max=5000) -> list:
         """
         Retourne la liste les stations et stocke le résultat brut JSON en mémoire.
         """
         parametres = {  "format": "json",
-                        "size": taille_max
+                        "size": taille_max,
+                        "nb_mesures_piezo_min" : nb_mesures_piezo_min
                         }
 
         # Ajout des filtres
+        if date_recherche:
+            parametres['date_recherche'] = date_recherche
         if code_dep:
             parametres["code_departement"] = str(code_dep)
         if code_region:
@@ -71,6 +84,16 @@ class Entite:
             return pd.DataFrame()
 
         df = pd.DataFrame(self.donnees_brutes)
+
+        # - Nettoyage des informations inutiles ?
+        Index(['code_bss', 'urn_bss', 'date_debut_mesure', 'date_fin_mesure',
+       'code_commune_insee', 'nom_commune', 'x', 'y', 'codes_bdlisa',
+       'urns_bdlisa', 'geometry', 'bss_id', 'altitude_station',
+       'nb_mesures_piezo', 'code_departement', 'nom_departement', 'libelle_pe',
+       'profondeur_investigation', 'codes_masse_eau_edl', 'noms_masse_eau_edl',
+       'urns_masse_eau_edl', 'date_maj'],
+      dtype='object')
+        colonnes_inutiles = ['','','']
 
         # 1. Formatage des dates. TODO : que faire de "date maj" ?
         colonnes_dates = ['date_debut_mesure', 'date_fin_mesure', 'date_maj']
