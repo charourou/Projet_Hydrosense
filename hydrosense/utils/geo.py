24cf7b60454fc2ef129740edc0f8e92315425f20
Maxime Richard <maxime.richard@gmail.com>
2026-06-03 23:45:36 +0200

    knn_idw function

--- hydrosense/utils/geo.py
+++ hydrosense/utils/geo.py
@@ -21,3 +21,32 @@ def calc_dist(x_in, y_in, x_out, y_out ) -> float:
     haversine_km = haversine_rad * EARTH_RADIUS
 
     return haversine_km
+
+def knn_idw(valeurs, distances) -> float:
+    """
+    Calcule la moyenne pondérée (IDW) pour une observation (1D).
+    """
+    v = np.asarray(valeurs, dtype=float)
+    d = np.asarray(distances, dtype=float)
+
+    masque = ~np.isnan(v)
+    if not masque.any():
+        return np.nan
+
+    v_valides, d_valides  = v[masque], d[masque]
+
+    # Calcul des poids (1 / d^2) avec sécurité pour une distance de 0
+    poids = np.where(d_valides == 0, 1e10, 1.0 / (d_valides ** 2))
+    return float(np.sum(v_valides * poids) / np.sum(poids))
+
+
+if __name__ == '__main__':
+
+    print(knn_idw([1 , 2 ,3 ],[1,5,10])
+      )
+
+    print(knn_idw([np.nan , 2 ,3 ],[1,5,10])
+      )
+
+    print(knn_idw([np.nan , np.nan ,np.nan ],[1,5,10])
+      )
