9a2ea15f689c9b4961afffe175c306a38cd65593
Maxime Richard <maxime.richard@gmail.com>
2026-06-03 18:00:02 +0200

    simple geo for floats

--- hydrosense/utils/geo.py
+++ hydrosense/utils/geo.py
@@ -3,15 +3,15 @@ import pandas as pd
 
 EARTH_RADIUS = 6371
 
-def calc_dist(df_in: pd.DataFrame, df_out: pd.DataFrame ) -> float:
+def calc_dist(x_in, y_in, x_out, y_out ) -> float:
     """
     Calculate the haversine and Manhattan distances between two
     points on the earth (specified in decimal degrees)
     Vectorized version for pandas df
     Computes distance in km
     """
-    lat_1_rad, lon_1_rad = np.radians(df_in['y']), np.radians(df_in['x'])
-    lat_2_rad, lon_2_rad = np.radians(df_out['y']), np.radians(df_out['x'])
+    lat_1_rad, lon_1_rad = np.radians(y_in), np.radians(x_in)
+    lat_2_rad, lon_2_rad = np.radians(y_out), np.radians(x_out)
 
     dlon_rad = lon_2_rad - lon_1_rad
     dlat_rad = lat_2_rad - lat_1_rad
