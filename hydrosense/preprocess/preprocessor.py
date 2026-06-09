95d15b7690a80251bb76902ae5777c9fdf50081c
Romain Huchet <romainhuchet@gmail.com>
2026-06-09 15:39:44 +0200

    df_w['RR_synth'] ajouté

--- hydrosense/preprocess/preprocessor.py
+++ hydrosense/preprocess/preprocessor.py
@@ -26,17 +26,23 @@ def preprocess_week(df: pd.DataFrame) -> pd.DataFrame:
     # S'assure que l'index est bien de type DatetimeIndex
     df.index = pd.to_datetime(df.index)
 
-    # Rééchantillonnage semaine
-    y_week = df[params.TARGET_COL].resample('W').mean()
 
-    # Feature engineering
-    df_w = pd.DataFrame(y_week)
+    df_resample = df.resample('W').agg({
+        params.TARGET_COL: 'mean',   # niveau nappe → moyenne
+        'RR_synth': 'sum'            # précipitations → cumul hebdomadaire
+    })
+
+    df_w = pd.DataFrame(df_resample)
+
     df_w['semaine'] = df_w.index.isocalendar().week
     df_w['lag_1'] = df_w['niveau_nappe_eau'].shift(1)
     df_w['lag_2'] = df_w['niveau_nappe_eau'].shift(2)
     df_w['lag_3'] = df_w['niveau_nappe_eau'].shift(3)
     df_w['lag_4'] = df_w['niveau_nappe_eau'].shift(4)
     df_w['lag_52'] = df_w['niveau_nappe_eau'].shift(52)
+    df_w['RR_synth'] = df_w['RR_synth']
+    #df_w['RR_lag_2'] = df_w['RR_synth'].shift(2)   # pluie il y a 2 semaines
+    #df_w['RR_moy_4w'] = df_w['RR_synth'].rolling(window=4).sum()  # cumul 4 semaines
 
     # Moyenne du niveau de la nappe sur les 3 derniers mois (tendance récente)
     df_w['moyenne_3w'] = df_w['niveau_nappe_eau'].shift(1).rolling(window=3).mean()
@@ -47,7 +53,7 @@ def preprocess_week(df: pd.DataFrame) -> pd.DataFrame:
     # IMPORTANT : Applique le .dropna() APRÈS avoir créé ces nouvelles variables
     df_w = df_w.dropna()
 
-    X = df_w[['semaine', 'lag_1', 'lag_2', 'lag_3','lag_4', 'lag_52', 'moyenne_3w', 'moyenne_6w']]
+    X = df_w[['semaine', 'lag_1', 'lag_2', 'lag_3','lag_4', 'lag_52', 'moyenne_3w', 'moyenne_6w','RR_synth']]
 
     y_target = df_w['niveau_nappe_eau']
 
