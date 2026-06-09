06b1993e8497edff16c90f6f159339e908b1b084
Maxime Richard <maxime.richard@gmail.com>
2026-06-09 19:31:59 +0200

    recherche des voisins piezos

--- hydrosense/utils/geo.py
+++ hydrosense/utils/geo.py
@@ -22,6 +22,7 @@ def calc_dist(x_in, y_in, x_out, y_out ) -> float:
 
     return haversine_km
 
+
 def knn_idw(valeurs, distances) -> float:
     """
     Calcule la moyenne pondérée (IDW) pour une observation (1D).
@@ -50,6 +51,53 @@ def appliquer_idw_df(df: pd.DataFrame, colonnes_cibles: list, distances: list) -
     )
 
     return series_idw
+
+
+def trouver_voisins_hydrogeologiques(df_catalogue: pd.DataFrame, bss_target: str, n_voisins: int = 10) -> pd.DataFrame:
+    """
+    Trouve les piézomètres voisins les plus pertinents pour une cible donnée.
+    Priorité : 1. Même Masse d'eau -> 2. Même Géologie (BD Lisa) -> 3. Distance géographique.
+    """
+
+    target_row = df_catalogue[df_catalogue['bss_id'] == bss_target]
+
+    set_eau_target = extraire_set(target_row['codes_masse_eau_edl'].values[0])
+    set_geo_target = extraire_set(target_row['codes_bdlisa'].values[0])
+    lat_target, lon_target = target_row['y'].values[0], target_row['x'].values[0]
+
+    df_candidats = df_catalogue[df_catalogue['bss_id'] != bss_target].copy()
+
+    # Scoring hydrogéologique
+    df_candidats['same_eau'] = df_candidats['codes_masse_eau_edl'].apply(lambda x: a_une_couche_commune(x, set_eau_target))
+    df_candidats['same_geo'] = df_candidats['codes_bdlisa'].apply(lambda x: a_une_couche_commune(x, set_geo_target))
+
+    # On ne garde que ceux qui ont au moins une correspondance
+    df_pertinents = df_candidats[df_candidats['same_eau'] | df_candidats['same_geo']].copy()
+
+    # Calcul de la distance
+    df_pertinents['distance_km'] = df_pertinents.apply(
+        lambda row: calc_dist(lat_target, lon_target, row['y'], row['x']), axis=1
+    )
+
+    # Tri multi-critères et sélection
+    df_tries = df_pertinents.sort_values(
+        by=['same_eau', 'same_geo', 'distance_km'],
+        ascending=[False, False, True]
+    )
+
+    return df_tries.head(n_voisins)
+
+
+def extraire_set(valeur_brute) -> set:
+    """Transforme une chaîne de codes séparés par des virgules en un set propre."""
+    if pd.isna(valeur_brute) or not valeur_brute:
+        return set()
+    return set([c.strip() for c in str(valeur_brute).split(',') if c.strip()])
+
+def a_une_couche_commune(valeur_candidat, set_cible: set) -> bool:
+    """Vérifie si les codes du candidat ont au moins un élément en commun avec la cible."""
+    set_candidat = extraire_set(valeur_candidat)
+    return len(set_cible.intersection(set_candidat)) > 0
 
 
 
